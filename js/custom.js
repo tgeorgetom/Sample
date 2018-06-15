036c6b082989cccdfaacca71381cf41124f89ab6
George Thomas <tgeorgetom@gmail.com>
2018-06-15 17:35:33 +0530

    Design Changes

--- js/custom.js
+++ js/custom.js
@@ -1,320 +1,373 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  function bg_img_adjust() {
+    var window_width = $(window).width();
+    if (window_width >= 767) {
+      var initial_left = $("#myNavbar li:first-child").position().left;
+      console.log(initial_left);
+      var first_width = $("#myNavbar li:first-child").outerWidth();
+      console.log(first_width);
+      $(".nav_line").show();
+      $(".nav_line").css({
+        left: initial_left + "px",
+        width: first_width + "px"
+      });
+    } else {
+      $(".nav_line").hide();
+      $(".nav_line").css({ left: "0", width: "0px" });
+    }
+  }
+  banner_height();
 
-	function bg_img_adjust(){
-		var window_width = $(window).width();
-		if (window_width >= 767 ) {
-			var initial_left = $('#myNavbar li:first-child').position().left;
-			console.log(initial_left);
-			var  first_width = $('#myNavbar li:first-child').outerWidth();
-			console.log(first_width);
-			$('.nav_line').show();
-			$('.nav_line').css({'left': initial_left+'px' , 'width' : first_width+'px'});
-		} else{
-			$('.nav_line').hide();
-			$('.nav_line').css({'left':'0' , 'width' : '0px' });
-		}
+  function banner_height() {
+    var window_width = $(window).width();
+    var window_height = $(window).height();
 
-	}
-banner_height();
-
-	function banner_height(){
-		var window_width = $(window).width();
-		var window_height = $(window).height();
-
-		if (window_width >= 767) {
-			$('.Banner').css({'height': window_height });
-		} else{
-			$('.Banner').css({'height': '700px' });
-		}
-	}
-
-	$(window).resize(function(){
-		banner_height();
-	});
-
-	gradient();
-});
-
-
-function gradient(){
-
-	var colors = new Array(
-	  [62,35,255],
-	  [60,255,60],
-	  [255,35,98],
-	  [45,175,230],
-	  [255,0,255],
-	  [255,128,0]);
-
-	var step = 0;
-	//color table indices for: 
-	// current color left
-	// next color left
-	// current color right
-	// next color right
-	var colorIndices = [0,1,2,3];
-
-	//transition speed
-	var gradientSpeed = 0.002;
-
-	function updateGradient()
-	{
-	  
-	  if ( $===undefined ) return;
-	  
-	var c0_0 = colors[colorIndices[0]];
-	var c0_1 = colors[colorIndices[1]];
-	var c1_0 = colors[colorIndices[2]];
-	var c1_1 = colors[colorIndices[3]];
-
-	var istep = 1 - step;
-	var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-	var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-	var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-	var color1 = "rgb("+r1+","+g1+","+b1+")";
-
-	var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-	var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-	var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-	var color2 = "rgb("+r2+","+g2+","+b2+")";
-
-	 $('.Banner , footer').css({
-	   background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-	    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
-	  
-	  step += gradientSpeed;
-	  if ( step >= 1 )
-	  {
-	    step %= 1;
-	    colorIndices[0] = colorIndices[1];
-	    colorIndices[2] = colorIndices[3];
-	    
-	    //pick two new target color indices
-	    //do not pick the same as the current one
-	    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-	    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-	    
-	  }
-	}
-
-	setInterval(updateGradient,10);
-}	
-
-
-
-particlesJS('canvas',
-  
-  {
-    "particles": {
-      "number": {
-        "value": 80,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 5,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 6,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "repulse"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true,
-    "config_demo": {
-      "hide_card": false,
-      "background_color": "#b61924",
-      "background_image": "",
-      "background_position": "50% 50%",
-      "background_repeat": "no-repeat",
-      "background_size": "cover"
+    if (window_width >= 767 && window_height <= 1000) {
+      $(".Banner").css({ height: window_height });
+    } else {
+      $(".Banner").css({ height: "700px" });
     }
   }
 
+  $(window).resize(function() {
+    banner_height();
+  });
+
+  gradient();
+});
+
+function gradient() {
+  var colors = new Array(
+    [62, 35, 255],
+    [60, 255, 60],
+    [255, 35, 98],
+    [45, 175, 230],
+    [255, 0, 255],
+    [255, 128, 0]
+  );
+
+  var step = 0;
+  //color table indices for:
+  // current color left
+  // next color left
+  // current color right
+  // next color right
+  var colorIndices = [0, 1, 2, 3];
+
+  //transition speed
+  var gradientSpeed = 0.002;
+
+  function updateGradient() {
+    if ($ === undefined) return;
+
+    var c0_0 = colors[colorIndices[0]];
+    var c0_1 = colors[colorIndices[1]];
+    var c1_0 = colors[colorIndices[2]];
+    var c1_1 = colors[colorIndices[3]];
+
+    var istep = 1 - step;
+    var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+    var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+    var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+    var color1 = "rgb(" + r1 + "," + g1 + "," + b1 + ")";
+
+    var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+    var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+    var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+    var color2 = "rgb(" + r2 + "," + g2 + "," + b2 + ")";
+
+    $(".Banner , footer")
+      .css({
+        background:
+          "-webkit-gradient(linear, left top, right top, from(" +
+          color1 +
+          "), to(" +
+          color2 +
+          "))"
+      })
+      .css({
+        background:
+          "-moz-linear-gradient(left, " + color1 + " 0%, " + color2 + " 100%)"
+      });
+
+    step += gradientSpeed;
+    if (step >= 1) {
+      step %= 1;
+      colorIndices[0] = colorIndices[1];
+      colorIndices[2] = colorIndices[3];
+
+      //pick two new target color indices
+      //do not pick the same as the current one
+      colorIndices[1] =
+        (colorIndices[1] +
+          Math.floor(1 + Math.random() * (colors.length - 1))) %
+        colors.length;
+      colorIndices[3] =
+        (colorIndices[3] +
+          Math.floor(1 + Math.random() * (colors.length - 1))) %
+        colors.length;
+    }
+  }
+
+  setInterval(updateGradient, 10);
+}
+
+particlesJS(
+  "canvas",
+
+  {
+    particles: {
+      number: {
+        value: 60,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      },
+      color: {
+        value: "#ffffff"
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000"
+        },
+        polygon: {
+          nb_sides: 5
+        },
+        image: {
+          src: "img/github.svg",
+          width: 100,
+          height: 100
+        }
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false
+        }
+      },
+      size: {
+        value: 5,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false
+        }
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 6,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200
+        }
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse"
+        },
+        onclick: {
+          enable: true,
+          mode: "push"
+        },
+        resize: true
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1
+          }
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3
+        },
+        repulse: {
+          distance: 200
+        },
+        push: {
+          particles_nb: 4
+        },
+        remove: {
+          particles_nb: 2
+        }
+      }
+    },
+    retina_detect: true,
+    config_demo: {
+      hide_card: false,
+      background_color: "#b61924",
+      background_image: "",
+      background_position: "50% 50%",
+      background_repeat: "no-repeat",
+      background_size: "cover"
+    }
+  }
 );
 
-
 function initMap() {
-        // Styles a map in night mode.
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 12.9747567, lng: 77.5741145},
-          zoom: 12,
-          styles: [
-            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-            {
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'geometry',
-              stylers: [{color: '#263c3f'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#6b9a76'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#38414e'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#212a37'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#9ca5b3'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#746855'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#1f2835'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#f3d19c'}]
-            },
-            {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
-            },
-            {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#17263c'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#515c6d'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
-            }
-          ]
-        });
-
-        var marker = new google.maps.Marker({
-          position: {lat: 12.9747567, lng: 77.5741145},
-          map: map,
-          title: 'Bengalore'
-        });
-
+  // Styles a map in night mode.
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 12.9747567, lng: 77.5741145 },
+    zoom: 12,
+    styles: [
+      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+      {
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }]
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [{ color: "#263c3f" }]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#6b9a76" }]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "#38414e" }]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#212a37" }]
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#9ca5b3" }]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#746855" }]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#1f2835" }]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#f3d19c" }]
+      },
+      {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [{ color: "#2f3948" }]
+      },
+      {
+        featureType: "transit.station",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }]
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{ color: "#17263c" }]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#515c6d" }]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#17263c" }]
       }
+    ]
+  });
+
+  var marker = new google.maps.Marker({
+    position: { lat: 12.9747567, lng: 77.5741145 },
+    map: map,
+    title: "Bangalore"
+  });
+}
+
+(function() {
+  function id(v) {
+    return document.getElementById(v);
+  }
+  function loadbar() {
+    var ovrl = id("overlay"),
+      prog = id("progress"),
+      stat = id("progstat"),
+      img = document.images,
+      c = 0;
+    tot = img.length;
+
+    function imgLoaded() {
+      c += 1;
+      var perc = (((100 / tot) * c) << 0) + "%";
+      prog.style.width = perc;
+      stat.innerHTML = "Loading " + perc;
+      if (c === tot) return doneLoading();
+    }
+    function doneLoading() {
+      // ovrl.style.opacity = 0;
+      setTimeout(function() {
+        ovrl.classList.add("hideOverlay");
+      }, 1200);
+    }
+    for (var i = 0; i < tot; i++) {
+      var tImg = new Image();
+      tImg.onload = imgLoaded;
+      tImg.onerror = imgLoaded;
+      tImg.src = img[i].src;
+    }
+  }
+  document.addEventListener("DOMContentLoaded", loadbar, false);
+})();
+
+$(window).scroll(function(event) {
+  var scroll = $(window).scrollTop();
+  if (!$(".Skills").hasClass("OnView")) {
+    $(".Skills").toggleClass("OnView", scroll >= $(".Skills").offset().top);
+  }
+});
+$(window).scroll();
+
+new WOW().init();
